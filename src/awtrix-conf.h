ae64c3392ff759982c90471ad06aa88b17dc5917
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2019-03-18 23:33:39 +0100

    first upload

---
+++ src/awtrix-conf.h
@@ -0,0 +1,11 @@
+///////////////////////// AWTRIXNODE CONFIG /////////////////////////
+
+// Wifi Config
+const char *ssid = "xxxx";
+const char *password = "xxxxx";
+char *awtrix_server = "192.168.178.39";
+
+// Node Config
+const char *nodename = "TestNode";
+int iconID = 6;
+const long interval = 30000;
